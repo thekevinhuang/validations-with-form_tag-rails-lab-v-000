ff6a7c39f11de5379c1425d149532a46af8b4964
Kevin Huang <yellow.khaos@gmail.com>
2018-06-17 16:34:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,37 @@
 class PostsController < ApplicationController
+  before_action :load_post, only: [:show, :edit, :update]
+
   def show
     @post = Post.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+
+  end
+
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def load_post
+    @post = Post.find(params[:id])
   end
 end
